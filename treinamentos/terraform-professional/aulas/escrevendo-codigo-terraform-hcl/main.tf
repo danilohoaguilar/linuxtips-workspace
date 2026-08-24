7e4eb258defb51758aa57409f192b9572ddb95a5
Danilo Ho Aguilar <danilo.ho.aguilar@gmail.com>
2026-08-24 12:06:48 -0300

    Variaves

--- treinamentos/terraform-professional/aulas/escrevendo-codigo-terraform-hcl/main.tf
+++ treinamentos/terraform-professional/aulas/escrevendo-codigo-terraform-hcl/main.tf
@@ -1,4 +1,4 @@
- data "aws_ami" "ubuntu" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
@@ -14,11 +14,38 @@
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "example" {
+data "aws_ami" "ubuntu-east" {
+  provider    = aws.east
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu-west" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Ubuntu-west-2"
+  }
+}
+
+resource "aws_instance" "ubuntu-east" {
+  provider      = aws.east
+  ami           = data.aws_ami.ubuntu-east.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Ubuntu-east"
   }
 }
